f1c53587dab3169db9e14ba3c39a04c19fa1a849
lilytastic <thebeaul@gmail.com>
2018-07-02 01:40:28 -0400

    Fix for pluralize pipe

--- src/app/shared/pipes/pluralize.pipe.ts
+++ src/app/shared/pipes/pluralize.pipe.ts
@@ -8,8 +8,15 @@ export class PluralizePipe implements PipeTransform {
   transform(token: string, value: any): any {
     let transform: string = token;
 
-    if (!transform.endsWith('s')) {
-      transform += 's';
+    if (value === 1) {
+      if (transform.endsWith('s')) {
+        transform = transform.substr(0, transform.length - 1);
+      }
+    }
+    else {
+      if (!transform.endsWith('s')) {
+        transform += 's';
+      }
     }
 
     return transform;
