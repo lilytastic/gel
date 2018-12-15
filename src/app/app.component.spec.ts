7710e36ad8204079f9124169c36ff23b44e50d0d
lilytastic <thebeaul@gmail.com>
2018-12-15 18:34:26 -0500

    Fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { StatusComponent } from '@status/status.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ReaderModule } from '@reader/reader.module';
+import { StoreModule } from '@ngrx/store';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -15,18 +16,21 @@ describe('AppComponent', () => {
       ],
       imports: [
         AppRoutingModule,
-        ReaderModule
+        ReaderModule,
+        StoreModule.forRoot({})
       ],
       providers: [
         { provide: APP_BASE_HREF, useValue: '/' }
       ]
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
+
   it(`should have as title 'gel'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
